8162d5d9d1234b0f57d951508c20c032943cb4b7
Isaiah Norton <isaiah.norton@gmail.com>
2014-02-06 00:21:45 -0500

    definition ordering

--- src/TimeArrays.jl
+++ src/TimeArrays.jl
@@ -7,13 +7,14 @@ using Datetime
 export TimeArray, 
        readtimearray,
        .+, .-, .*, ./  #, .>, .<, .>=, .<=, .== # I think these should return Bool
-
+       
 #################################
 ###### include ##################
 #################################
 
 include("io.jl")
 include("timearray.jl")
+include("operators.jl")
 include("timestamp.jl")
 include("transformations.jl")
 include("utilities.jl")
